ee76e543ffcf99c90ee39e8c2effd140327b0c58
Ariyalex <ariya17duta@gmail.com>
2026-06-02 08:37:53 +0700

    fix: update dotenv file path and remove .env from assets

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:sejasa/my_app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: 'assets/env');
 
   await DependencyInjection.init();
 
